b427db5b9f12ec011e4c01dd94cb87c57e65b4d5
hewtungyuen <hewtungyuen@gmail.com>
2023-03-07 22:39:54 +0800

    add barebones submit functionality

--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -1,13 +1,29 @@
 import { Button, Stack, TextField, Typography } from "@mui/material";
+import { useState } from "react";
 
 function Input() {
+  const [url, setUrl] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(url);
+  };
+  
   return (
     <>
       <Typography variant="h5">Input</Typography>
-      <Stack direction={'row'} justifyContent={'center'} spacing={1}>
-        <TextField fullWidth label="Enter url here" />
-        <Button variant="contained">Shorten</Button>
-      </Stack>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <Stack direction={"row"} justifyContent={"center"} spacing={1}>
+          <TextField
+            onChange={(event) => setUrl(event.target.value)}
+            fullWidth
+            label="Enter url here"
+          />
+          <Button type="submit" variant="contained">
+            Shorten
+          </Button>
+        </Stack>
+      </form>
     </>
   );
 }
