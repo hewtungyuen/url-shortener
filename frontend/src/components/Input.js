6da0ab938e7a4890693c1a0f391ab36229a79ae1
hewtungyuen <hewtungyuen@gmail.com>
2023-03-07 21:24:44 +0800

    create basic input box with button

--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -1,9 +1,14 @@
-import { Typography } from "@mui/material";
+import { Button, Stack, TextField, Typography } from "@mui/material";
 
 function Input() {
   return (
     <>
-      <Typography>Input</Typography>
+      <Typography variant="h5">Input</Typography>
+
+      <Stack direction={'row'} justifyContent={'center'} spacing={1}>
+        <TextField fullWidth label="Enter url here" />
+        <Button variant="contained">Shorten</Button>
+      </Stack>
     </>
   );
 }
