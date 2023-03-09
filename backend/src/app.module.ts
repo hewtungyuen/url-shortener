c3353546b696b5969f1a5aae54761fc324e6fa1f
hewtungyuen <hewtungyuen@gmail.com>
2023-03-09 14:56:31 +0800

    initialise db connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Urls } from './typeorm/Urls';
 import { UrlModule } from './url/url.module';
+require('dotenv').config();
 
 @Module({
-  imports: [UrlModule],
+  imports: [
+    UrlModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: [Urls],
+      synchronize: process.env.TYPEORM_SYNC === 'true',
+    }),
+  ],
   controllers: [],
   providers: [],
 })
