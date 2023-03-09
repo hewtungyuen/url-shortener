b2a2a057d69c542867fb8d86505eb6b3f0c57ecd
hewtungyuen <hewtungyuen@gmail.com>
2023-03-09 17:20:27 +0800

    link submit functionality to backend

--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -1,20 +1,22 @@
 import { Button, Stack, TextField, Typography } from "@mui/material";
 import { useState } from "react";
-
+import api from "../axiosConfig";
 function Input() {
   const [url, setUrl] = useState("");
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(url);
+    await api.post(`/url/${url}`);
+    setUrl("");
   };
-  
+
   return (
     <>
       <Typography variant="h5">Input</Typography>
       <form noValidate autoComplete="off" onSubmit={handleSubmit}>
         <Stack direction={"row"} justifyContent={"center"} spacing={1}>
           <TextField
+            value={url}
             onChange={(event) => setUrl(event.target.value)}
             fullWidth
             label="Enter url here"
