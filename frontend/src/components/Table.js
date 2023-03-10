eca2a9d349139dfd16de1eac8a8c13bddb5ebe3e
hewtungyuen <hewtungyuen@gmail.com>
2023-03-10 12:44:39 +0800

    add snackbar for copy to clipboard and reformat

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -11,6 +11,8 @@ import api from "../axiosConfig";
 import { styled } from "@mui/material/styles";
 import DeleteIcon from "@mui/icons-material/Delete";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
 
 const BoldCell = styled(TableCell)({
   fontWeight: "bold",
@@ -18,66 +20,87 @@ const BoldCell = styled(TableCell)({
 });
 
 export default function BasicTable({ urls, refresh, setRefresh }) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   const handleDelete = async (id) => {
     setRefresh(!refresh);
     await api.delete(`/url/${id}`);
   };
 
   return (
-    <TableContainer component={Paper}sx={{ boxShadow: 6 }}>
-      <Table
-        sx={{ minWidth: 650, boxShadow: 6 }}
-        aria-label="simple table"
-      >
-        <colgroup>
-          <col style={{ width: "15%" }} />
-          <col style={{ width: "35%" }} />
-          <col style={{ width: "35%" }} />
-          <col style={{ width: "15%" }} />
-        </colgroup>
-        <TableHead sx={{ backgroundColor: "#744850" }}>
-          <BoldCell>Date</BoldCell>
-          <BoldCell>Original URL</BoldCell>
-          <BoldCell>Shortened URL</BoldCell>
-          <BoldCell>Actions</BoldCell>
-        </TableHead>
-        <TableBody>
-          {urls &&
-            urls.map((row) => (
-              <TableRow
-                sx={{
-                  "&:nth-of-type(odd)": { backgroundColor: "#DEDEDE" },
-                  "&:last-child td, &:last-child th": { border: 0 },
-                }}
-                key={row.id}
-              >
-                <TableCell component="th" scope="row">
-                  {row.date.slice(0, 10)}
-                </TableCell>
-                <TableCell>{row.long_url}</TableCell>
-                <TableCell>
-                  <a href={row.long_url}>{row.id}</a>
-                </TableCell>
-                <TableCell>
-                  <ButtonGroup variant="outlined">
-                    <IconButton onClick={() => handleDelete(row.id)}>
-                      <DeleteIcon />
-                    </IconButton>
-                    <IconButton
-                      onClick={() =>
-                        navigator.clipboard.writeText(
-                          `${process.env.REACT_APP_BASE_API}/url/${row.id}`
-                        )
-                      }
-                    >
-                      <ContentCopyIcon />
-                    </IconButton>
-                  </ButtonGroup>
-                </TableCell>
-              </TableRow>
-            ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+        <MuiAlert
+          onClose={handleClose}
+          severity="success"
+          sx={{ width: "100%" }}
+        >
+          Copied to clipboard!
+        </MuiAlert>
+      </Snackbar>
+      <TableContainer component={Paper} sx={{ boxShadow: 6 }}>
+        <Table sx={{ minWidth: 650, boxShadow: 6 }} aria-label="simple table">
+          <colgroup>
+            <col style={{ width: "15%" }} />
+            <col style={{ width: "35%" }} />
+            <col style={{ width: "35%" }} />
+            <col style={{ width: "15%" }} />
+          </colgroup>
+          <TableHead sx={{ backgroundColor: "#744850" }}>
+            <TableRow>
+              <BoldCell>Date</BoldCell>
+              <BoldCell>Original URL</BoldCell>
+              <BoldCell>Shortened URL</BoldCell>
+              <BoldCell>Actions</BoldCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {urls &&
+              urls.map((row) => (
+                <TableRow
+                  sx={{
+                    "&:nth-of-type(odd)": { backgroundColor: "#DEDEDE" },
+                    "&:last-child td, &:last-child th": { border: 0 },
+                  }}
+                  key={row.id}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.date.slice(0, 10)}
+                  </TableCell>
+                  <TableCell>{row.long_url}</TableCell>
+                  <TableCell>
+                    <a href={row.long_url}>{row.id}</a>
+                  </TableCell>
+                  <TableCell>
+                    <ButtonGroup variant="outlined">
+                      <IconButton onClick={() => handleDelete(row.id)}>
+                        <DeleteIcon />
+                      </IconButton>
+                      <IconButton
+                        onClick={() => {
+                          navigator.clipboard.writeText(
+                            `${process.env.REACT_APP_BASE_API}/url/${row.id}`
+                          );
+                          setOpen(true);
+                        }}
+                      >
+                        <ContentCopyIcon />
+                      </IconButton>
+                    </ButtonGroup>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
